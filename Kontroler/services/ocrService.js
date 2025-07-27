b84bac71bc6a11c1b071b1b263a3c7de3154f0ae
thash <thashmail@gmail.com>
2025-07-27 22:37:31 +0200

    Zmiana gammy na 4.0

--- Kontroler/services/ocrService.js
+++ Kontroler/services/ocrService.js
@@ -53,17 +53,17 @@ class OCRService {
      * @returns {string} - Ścieżka do przetworzonego obrazu
      */
     async preprocessWhiteTextOnGray(imagePath, outputPath) {
-        logger.info('Użycie ustawień OCR z Rekrutera dla ataku z korekcją gamma 2.5');
+        logger.info('Użycie ustawień OCR z Rekrutera dla ataku z korekcją gamma 4.0');
         
         await sharp(imagePath)
-            .gamma(2.5)
+            .gamma(4.0)
             .grayscale()
             .threshold(200)
             .negate()
             .png()
             .toFile(outputPath);
         
-        logger.info('Preprocessing dla białego tekstu zakończony (styl Rekruter - atak + gamma 2.5)');
+        logger.info('Preprocessing dla białego tekstu zakończony (styl Rekruter - atak + gamma 4.0)');
         return outputPath;
     }
 
